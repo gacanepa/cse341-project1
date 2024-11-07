a43101354afdd7c4ebda69bd89982bd8a779765e
Gabriel Cánepa <gacanepa@gmail.com>
2024-11-06 21:01:24 -0300

    Use contacts routes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import indexRoutes from './routes/index.js';
+import contactsRoutes from './routes/contacts.js';
+
 const app = express();
 
 const port = process.env.PORT || 3001;
 
 app.use('/', indexRoutes);
+app.use('/contacts', contactsRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
