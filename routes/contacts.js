d4df36c9a18b3042998c4f764a9f361422cc88fb
Gabriel Cánepa <gacanepa@gmail.com>
2024-11-16 13:12:26 -0300

    Add routes for creating, updated, and soft-deleting contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
-import { getAllContacts, getContactById } from '../controllers/contacts.js';
+import {
+  createContact,
+  getAllContacts,
+  getContactById,
+  softDeleteContact,
+  updateContact,
+} from '../controllers/contacts.js';
 
 const router = Router();
 
 router.get('/', getAllContacts);
 router.get('/:id', getContactById);
+router.post('/', createContact);
+router.put('/:id', updateContact);
+router.delete('/:id', softDeleteContact);
 
 export default router;
