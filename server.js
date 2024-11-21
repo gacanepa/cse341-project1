ae332c55110cbd06275656a73ece87c0811ebf74
Gabriel Cánepa <gacanepa@gmail.com>
2024-11-21 17:49:44 -0300

    Use error handlers in the main server file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { errorHandler, notFoundErrorHandler } from './middleware/index.js';
 import indexRoutes from './routes/index.js';
 import contactsRoutes from './routes/contacts.js';
 import docsRoutes from './routes/docs.js';
@@ -16,6 +17,9 @@ const port = process.env.PORT || 3001;
 app.use('/', indexRoutes);
 app.use('/contacts', contactsRoutes);
 app.use('/docs', docsRoutes);
+
+app.use(notFoundErrorHandler);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
